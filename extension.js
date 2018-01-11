0139b212e1accad838083cd511e37912270c8104
Karan Sharma <karansharma1295@gmail.com>
2018-01-11 21:04:03 +0530

    add login command with request token parameter

--- extension.js
+++ extension.js
@@ -2,47 +2,51 @@
 // Import the module and reference it with the alias vscode in your code below
 var KiteConnect = require("kiteconnect").KiteConnect;
 const vscode = require('vscode');
-
 const config = vscode.workspace.getConfiguration('vscode-kite')
+var kc = new KiteConnect(config['api_key']);
+const KiteTicker = require("kiteconnect").KiteTicker;
 
-const kc = new KiteConnect(config['api_key']);
-kc.setAccessToken(config['access_token'])
-
-var KiteTicker = require("kiteconnect").KiteTicker;
-var ticker = new KiteTicker(config['api_key'], 'ZV3952', '90e4a6d6913badded9bfe70345604cc9');
-
-ticker.connect();
-ticker.on("tick", setTick);
-ticker.on("connect", subscribe);
-
+function fetchMargin() {
+    kc.margins("equity")
+    .then(function(response) {
+        vscode.window.showInformationMessage(`Margin: ${response.data['net']} `);
+    }).catch(function(err) {
+        // Something went wrong.
+        vscode.window.showErrorMessage(err.message)
+    });
+}
 function setTick(ticks) {
     let ltp_s = `Ticks : ${ticks[0]['LastTradedPrice']}`;
-    // vscode.window.setStatusBarMessage(ticks[0]['LastTradedPrice']);
     console.log(ltp_s);
-    vscode.window.setStatusBarMessage(ltp_s);
+    console.log(ticks)
 }
 
 function subscribe() {
-	var items = [738561];
+    let items = config['live_stocks'];
+    console.log(items)
 	ticker.subscribe(items);
-	ticker.setMode(ticker.modeFull, items);
+	ticker.setMode(ticker.modeLTP, items);
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
+    let login = vscode.commands.registerCommand('extension.initializeKite', function () {
+        console.log("Request is: ", config['request_token'])
+        console.log("Secret is: ", config['api_secret'])
+        console.log("API Key: ", config['api_key'])
+        kc.requestAccessToken(config['request_token'], config['api_secret'])
+        .then(function(response) {
+            vscode.window.showInformationMessage("Logged In");
+            // let ticker = new KiteTicker(config['api_key'], response['user_id'], response['public_token']);
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-kite" is now active!');
-    console.log("Kite instance:", kc)
+        })
+        .catch(function(err) {
+            console.log("something is wrong", err)
+            vscode.window.showErrorMessage(err.message);
+        })
+    
+    });
 
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let quote = vscode.commands.registerCommand('extension.getQuote', function (stock) {
-        // The code you place here will be executed every time your command is executed
         vscode.window.showInformationMessage(stock);
         for (let s of config['symbols']) {
             kc.quote(config['exchange'], s)
@@ -53,33 +57,30 @@ function activate(context) {
                 let ltp = response.data['last_price']
                 let high = response.data.ohlc['high']
                 let low = response.data.ohlc['low']
-                // vscode.window.setStatusBarMessage("LOL");
                 vscode.window.showInformationMessage(`${s} LTP: ${ltp} Open: ${open} High: ${high} Low: ${low} Close: ${close}`);
             }).catch(function(err) {
-                vscode.window.showErrorMessage(err);
+                vscode.window.showErrorMessage(err.message);
             });
         }
-        // Display a message box to the user
     });
+    let stream = vscode.commands.registerCommand('extension.streamData', function () {
+        ticker.connect();
+        ticker.on("tick", setTick);
+        ticker.on("connect", subscribe);
+    });
+
     let margin = vscode.commands.registerCommand('extension.fetchMargin', function () {
-        // vscode.window.setStatusBarMessage("ROFL");
-        // The code you place here will be executed every time your command is executed
-            kc.margins("equity")
-            .then(function(response) {
-                console.log("Margin:", response)
-                vscode.window.showInformationMessage(`Margin: ${response.data['net']} `);
-            }).catch(function(err) {
-                vscode.window.showInformationMessage(err);
-            });
+        fetchMargin()
         
     });
     context.subscriptions.push(margin);
+    context.subscriptions.push(login);
     context.subscriptions.push(quote);
+    context.subscriptions.push(stream);
 
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;
