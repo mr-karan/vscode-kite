25508435b8f965a0a13c89bc8d5ff7b729785cdc
Karan Sharma <karansharma1295@gmail.com>
2018-01-12 17:41:44 +0530

    Add websocket support

--- extension.js
+++ extension.js
@@ -1,82 +1,96 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-var KiteConnect = require("kiteconnect").KiteConnect;
 const vscode = require('vscode');
-const config = vscode.workspace.getConfiguration('vscode-kite')
-var kc = new KiteConnect(config['api_key']);
 const KiteTicker = require("kiteconnect").KiteTicker;
+const KiteConnect = require("kiteconnect").KiteConnect;
+let config = vscode.workspace.getConfiguration('vscode-kite');
+let kc = new KiteConnect(config['api_key']);
+let store = require('store')
+
+
+function refresh(){
+    config = vscode.workspace.getConfiguration('vscode-kite')
+}
 
 function fetchMargin() {
     kc.margins("equity")
     .then(function(response) {
         vscode.window.showInformationMessage(`Margin: ${response.data['net']} `);
     }).catch(function(err) {
-        // Something went wrong.
         vscode.window.showErrorMessage(err.message)
     });
 }
-function setTick(ticks) {
-    let ltp_s = `Ticks : ${ticks[0]['LastTradedPrice']}`;
-    console.log(ltp_s);
-    console.log(ticks)
+
+function fetchQuote(tradingSymbol) {
+    kc.quote(config['exchange'], tradingSymbol)
+    .then(function(response) {
+        let open = response.data.ohlc['open']
+        let close= response.data.ohlc['close']
+        let ltp = response.data['last_price']
+        let high = response.data.ohlc['high']
+        let low = response.data.ohlc['low']
+        vscode.window.showInformationMessage(`${tradingSymbol} LTP: ${ltp} Open: ${open} High: ${high} Low: ${low} Close: ${close}`);
+    }).catch(function(err) {
+        vscode.window.showErrorMessage(err.message);
+    });
 }
 
-function subscribe() {
-    let items = config['live_stocks'];
-    console.log(items)
-	ticker.subscribe(items);
-	ticker.setMode(ticker.modeLTP, items);
+function streamStock() {
+    user_data = store.get('user_data')
+    let ticker = new KiteTicker(config['api_key'], user_data['user_id'], user_data['public_token']);
+    ticker.connect();
+    ticker.on("tick", setTick);
+    ticker.on("connect", subscribe);
+
+    function setTick(ticks) {
+        for (let tick of ticks) {
+            let status = vscode.window.createStatusBarItem(1,100)
+            status.text = `${tick['Token']} : LTP ${tick['LastTradedPrice']}`
+            status.show()
+            console.log("Ticks", ticks);
+        }
+    }
+
+    function subscribe() {
+        let items = config['live_stocks'];
+        ticker.subscribe(items);
+        ticker.setMode(ticker.modeFull, items);
+    }
+
 }
 
 function activate(context) {
     let login = vscode.commands.registerCommand('extension.initializeKite', function () {
-        console.log("Request is: ", config['request_token'])
-        console.log("Secret is: ", config['api_secret'])
-        console.log("API Key: ", config['api_key'])
         kc.requestAccessToken(config['request_token'], config['api_secret'])
         .then(function(response) {
+            console.log(response)
             vscode.window.showInformationMessage("Logged In");
-            // let ticker = new KiteTicker(config['api_key'], response['user_id'], response['public_token']);
-
+            store.set('user_data', {public_token:response.data['public_token'], user_data:response.data['user_id']});
         })
         .catch(function(err) {
-            console.log("something is wrong", err)
             vscode.window.showErrorMessage(err.message);
         })
-    
     });
 
     let quote = vscode.commands.registerCommand('extension.getQuote', function (stock) {
         vscode.window.showInformationMessage(stock);
-        for (let s of config['symbols']) {
-            kc.quote(config['exchange'], s)
-            .then(function(response) {
-                console.log(response)
-                let open = response.data.ohlc['open']
-                let close= response.data.ohlc['close']
-                let ltp = response.data['last_price']
-                let high = response.data.ohlc['high']
-                let low = response.data.ohlc['low']
-                vscode.window.showInformationMessage(`${s} LTP: ${ltp} Open: ${open} High: ${high} Low: ${low} Close: ${close}`);
-            }).catch(function(err) {
-                vscode.window.showErrorMessage(err.message);
-            });
+        for (let tradingSymbol of config['symbols']) {
+            fetchQuote(tradingSymbol)        
         }
     });
-    let stream = vscode.commands.registerCommand('extension.streamData', function () {
-        ticker.connect();
-        ticker.on("tick", setTick);
-        ticker.on("connect", subscribe);
+
+    let stream = vscode.commands.registerCommand('extension.streamData', function (stock) {
+        streamStock()
     });
+
 
     let margin = vscode.commands.registerCommand('extension.fetchMargin', function () {
         fetchMargin()
-        
     });
+
     context.subscriptions.push(margin);
     context.subscriptions.push(login);
-    context.subscriptions.push(quote);
     context.subscriptions.push(stream);
+    context.subscriptions.push(quote);
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(refresh))
 
 }
 exports.activate = activate;
